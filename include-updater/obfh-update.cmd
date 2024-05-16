1b7f3d4e69e520173da42acf9d5ce020a1bb36e2
DosX <admin@kay-software.ru>
2024-05-16 22:46:46 +0300

    Update obfh-update.cmd

--- include-updater/obfh-update.cmd
+++ include-updater/obfh-update.cmd
@@ -1,29 +1,53 @@
 @echo off
 echo.
+
+:: Host and protocol
 set "protocol=https"
 set "host=github.com:443"
-set "obfus_h_path=DosX-dev/raw/main/include/obfus.h"
 
-:: Current directory
-set "output_dir=./"
+:: Path to file of obfus.h
+set "obfus_h_path=DosX-dev/obfus.h/raw/main/include/obfus.h"
 
-set "special=--ssl-no-revoke -s"
+:: Save to directory...
+:: "\." - current directory (.\obfus.h)
+set "output_dir=.\"
 
 :: If error:
-::  0 - pause
+::  0 - stop
 ::  1 - continue (not recommended)
 set "onerror=0"
 
+:: Additional cURL options
+set "special=--ssl-no-revoke -s"
+
+
+
+call :process
+set /a process_el=errorlevel
+
+if "%onerror%" == "0" if "%errorlevel%" == "1" ( timeout 1 /nobreak>nul && call:log "STOPPED!" && pause>nul )
+
+exit /b %process_el%
+
+:process
+set "curl_path=%windir%\System32\curl.exe"
 call:log "Getting the latest version of obfus.h protection from %host%..."
-curl -L %special% "%protocol%://%host%/%obfus_h_path%" -o "%output_dir%\obfus.h"
-if "%errorlevel%" == "0" (
-    call:log "Latest version of protection has been installed."
-) else (
-    call:log "An error occurred while downloading the component package."
-    if "%onerror%" == "0" pause>nul
+
+if not exist "%curl_path%" (
+    call:log "Missing cURL executable at %curl_path%!"
+    exit /b 1    
 )
 
-exit /b 0
+call "%curl_path%" -L %special% "%protocol%://%host%/%obfus_h_path%" -o "%output_dir%\obfus.h"
+
+if "%errorlevel%" == "0" (
+    call:log "Latest version of protection has been installed."
+    exit /b 0
+) else (
+    call:log "An error occurred while downloading the component package."
+    exit /b 1
+)
+goto :eof
 
 :log
 echo [%time%][pkg:obfus.h] %~1
