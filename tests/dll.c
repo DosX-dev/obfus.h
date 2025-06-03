1d8fa72ee20f93b26a45dbb997974d06c675a116
Hors <horsicq@gmail.com>
2025-06-04 00:51:44 +0200

    Update dll.c
    Add comments

--- tests/dll.c
+++ tests/dll.c
@@ -4,11 +4,13 @@
 //
 
 #include <windows.h>
-
 #include "../include/obfus.h"
 
+// Exported data
 __declspec(dllexport) const char *hello_data = "(not set)";
 
-__declspec(dllexport) void hello_func(void) {
-    MessageBox(0, hello_data, "From DLL", MB_ICONINFORMATION);
+// Exported function
+__declspec(dllexport) void hello_func(void)
+{
+    MessageBoxA(NULL, hello_data, "From DLL", MB_ICONINFORMATION);
 }
